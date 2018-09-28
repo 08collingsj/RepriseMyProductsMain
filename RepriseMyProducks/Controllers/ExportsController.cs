39f4a0efe4f904922e69f69303801ac55fbfdced
COLLINGS <Q5081793@live.tees.ac.uk>
2018-09-28 14:49:35 +0100

    Add project files.

---
+++ RepriseMyProducks/Controllers/ExportsController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace RepriseMyProducks.Controllers
+{
+    public class ExportsController : ApiController
+    {
+        private Producks.Model.StoreDb db = new Producks.Model.StoreDb();
+
+        // GET: api/Brands
+        [HttpGet]
+        [Route("api/Brands")]
+        public IEnumerable<Dtos.Brand> GetBrands()
+        {
+
+            return db.Brands
+                     .AsEnumerable()
+                     .Select(b => new Dtos.Brand
+                     {
+                        Id = b.Id,
+                        Name = b.Name,
+                        Active = b.Active
+                     });
+        }
+
+        //Get: api/Categories
+        [HttpGet]
+        [Route("api/AllBrands")]
+        public IEnumerable<Dtos.Category> GetCategories()
+        {
+            return db.Categories
+                .AsEnumerable()
+                .Select(b => new Dtos.Category
+                {
+                    Name = b.Name,
+                    Description = b.Description,
+                    Active = b.Active
+                });
+        }
+
+        //Get: api/Products
+        [HttpGet]
+        [Route("api/Products")]
+        public IEnumerable<Dtos.Product> GetProducts()
+        {
+            return db.Products.AsEnumerable()
+                .Select(p => new Dtos.Product
+                {
+                    Name = p.Name,
+                    Description = p.Description,
+                    Price = p.Price,
+                    StockLevel = p.StockLevel,
+                    Active = p.Active
+                });
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ RepriseMyProducks/Dtos/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RepriseMyProducks.Dtos
+{
+    public class Product
+    {
+        public virtual string Name { get; set; }
+
+        public virtual string Description { get; set; }
+
+        public virtual double Price { get; set; }
+
+        public virtual int StockLevel { get; set; }
+
+        public virtual bool Active { get; set; }
+    }
+}
